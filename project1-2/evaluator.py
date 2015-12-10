c70954843f9e70e9fb71c8d0e635c4035d287101
anarchih <anarchih.tw@gmail.com>
2015-12-11 00:26:08 +0800

    implement huff model

--- project1-2/evaluator.py
+++ project1-2/evaluator.py
@@ -2,6 +2,7 @@
 import datetime as dt
 # import matplotlib.pyplot as plt
 import csv
+from random import random
 
 class Hospital(object):
     def __init__(self, name, x, y, min_capacity, max_capacity):
@@ -15,6 +16,8 @@ class Evaluater(object):
         self.data = self.resolve_data_points(filename)
         self.hospital = self.resolve_hospital("hospital.tsv")
         self.dist = dist
+
+        self.nearest_hospital = self.calc_nearest_hospital(self.data, self.hospital)
         self.total_capacity = total_capacity
         self.date_range = dt.timedelta(date_range)
         self.labels_ = [0 for i in range(len(self.data['x']))]
@@ -35,21 +38,74 @@ class Evaluater(object):
         f = open(filename, "r")
         f.readline()
         for row in csv.reader(f, delimiter='\t'):
-            h_list.append(Hospital(row[0], row[1], row[2], 0, 1000))
+            h_list.append(Hospital(row[0], float(row[2]), float(row[1]), 0, 100))
         return h_list
 
+    def calc_nearest_hospital(self, data, hospital):
+        nearest = [0] * len(data['x'])
+        for i, p1 in enumerate(zip(self.data['x'], self.data['y'])):
+            min_dist = 100000
+            for j, hos in enumerate(hospital):
+                dist = self.distance(p1, (hos.x, hos.y))
+                if dist < min_dist and dist < self.dist:
+                    min_dist = dist
+                    nearest[i] = j + 1
+        return nearest
+
+    def nearest(self, i, p, ind):
+        alpha = 1
+        beta = 1
+        dist_list = [0] * self.k_hospitals
+        s = 0
+        for j in range(self.k_hospitals):
+            d = self.distance(p, (self.hospital[j].x, self.hospital[j].y))
+            if d <= self.dist and ind[j] > 0:
+                dist_list[j] = d
+                s += ind[j] ** alpha / dist_list[j] ** beta
+            elif d == 0:
+                return j + 1
+            else:
+                dist_list[j] = -1.
+
+        p = 0
+        r = random()
+        for j, hos in enumerate(self.hospital):
+            if dist_list[j] != -1.:
+                p += ind[j] ** alpha / dist_list[j] ** beta / s
+                if r < p:
+                    return j + 1
+        return 0
+
     def eval(self, ind):
+        count = 0
+        latest_date = [dt.datetime(1990, 1, 1)] * self.k_hospitals
+        sum_capacity = [0] * self.k_hospitals
+        tmp = [[0] * self.date_range.days for i in range(self.k_hospitals)]
+        for i, p1 in enumerate(zip(self.data['x'], self.data['y'])):
+            c = self.nearest(i, p1, ind)
+            if c != 0:
+                c -= 1
+                date_gap = self.data['date'][i] - latest_date[c]
+                latest_date[c] = self.data['date'][i]
+                if date_gap >= self.date_range:
+                    sum_capacity[c] = 1
+                    tmp[c] = [0] * self.date_range.days
+                    tmp[c][0] = 1
+                    count += 1
+                else:
+                    t = [0] * date_gap.days + tmp[c][0:self.date_range.days - date_gap.days]
+                    t[0] += 1
+                    sum_c = sum(t)
+                    if sum_c <= ind[c]:
+                        tmp[c] = t
+                        sum_capacity[c] = sum_c
+                        count += 1
+        return count,
+
         return 1,
 
-    def find_data_belongs_to(self, p1, ind):
-        current_cluster = 10000
-        Min = 10000
-        for j, p2 in enumerate(ind):
-            dist = self.distance(p1, p2)
-            if dist < self.dist and dist < Min:
-                Min = dist
-                current_cluster = j
-        return current_cluster
+    def find_data_nearest_to(self, i, p, ind):
+        return self.nearest_hospital[i]
 
     def distance(self, p1, p2):
         return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
